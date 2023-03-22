c969566aad44fc1c4d36abe5c43a44f9df62dfee
0x000001A4 <ricky420@ricky>
2023-03-22 13:01:29 +0000

    changes in defclass

--- jmop.jl
+++ jmop.jl
@@ -1,4 +1,8 @@
-macro defclass(name, superclasses, slots)
+abstract type Top end
+abstract type Object <: Top end
+abstract type Class end
+
+macro defclass(name, superclasses, slots, kwargs...)
     begin
         println("Executing @defclass macro with arguments: ")
         println("Name of the class: $(string(name))")
@@ -6,10 +10,22 @@ macro defclass(name, superclasses, slots)
         println("Slots: $(string(slots))")
         _supers = eval(superclasses)
         _slots = eval(slots)
+
+        append!(_supers, [Object])
+
+        _meta = Class
+        _kwargs = eval(kwargs)
+        if length(kwargs) > 1
+            error("There should be at most 1 kwarg with the name metaclass")
+        elseif length(kwargs) == 1 && _kwargs[1].args[1] == :metaclass && typeof(_kwargs[1].args[2]) == Symbol
+            _meta = eval(_kwargs[1].args[2])
+        end
+
         quote
-            struct $(length(_supers) > 0 ? Expr(:(<:), :name, Union{_supers...}) : esc(name))
+            struct $(Expr(:(<:), esc(name), Union{_supers...}))
                 $(for slot in _slots println(slot) end)
             end
         end
+
     end
 end
