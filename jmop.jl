016d9667b24b573d7af582243c59db82170213e5
0x000001A4 <ricky420@ricky>
2023-04-02 22:27:59 +0100

    Metaobject abstraction to more fine-grain control over prints

--- jmop.jl
+++ jmop.jl
@@ -2,23 +2,54 @@ import Base: ==
 import Base.show
 using DataStructures
 
-function isMetaobject(object)
-    # Might need 1 more class_of
-    if isa(object, Vector{Any})
-        if length(object) == 0 || !isa(class_of(object), Vector{Any}) return false end
-        return class_of(class_of(object)) === Class
-    end
-    return false
+struct Metaobject
+    value::Vector{Any}
 end
 
-function compareMetaobjects(metaobject1::Vector{Any}, metaobject2::Vector{Any}, compared=Set{Tuple{Vector{Any},Vector{Any}}})
-    print("m1: $metaobject1, m2: $metaobject2")
+function Base.getindex(instance::Metaobject, index::Int64)
+    getfield(instance, :value)[index]
+end
 
+function Base.setindex!(instance::Metaobject, value::Any, index::Int64)
+    setindex!(getfield(instance, :value), value, index)
+end
+
+class_of(instance::Metaobject) = instance[1]
+
+function Base.getproperty(instance::Metaobject, property::Symbol)
+    if property == :value
+        throw(ArgumentError("The value field should not be accessed directly."))
+    else
+        instance[1+findall(slot -> slot == property, class_of(instance)[6])[1]]
+    end
+end
+
+function Base.setproperty!(instance::Metaobject, property::Symbol, value::Any)
+    if property == :value
+        throw(ArgumentError("The value field should not be accessed directly."))
+    else
+        instance[1+findall(slot -> slot == property, class_of(instance))[1]] = value
+    end
+end
+
+class_name(instance::Metaobject) = instance.name
+class_direct_slots(instance::Metaobject) = instance.direct_slots
+class_slots(instance::Metaobject) = instance.slots
+class_direct_superclasses(instance::Metaobject) = instance.direct_superclasses
+class_cpl(instance::Metaobject) = instance.cpl
+class_direct_methods(instance::Metaobject) = instance.direct_methods
+
+generic_methods(generic_function::Metaobject) = generic_function.methods
+method_specializers(method::Metaobject) = method.specializers
+
+isMetaobject(object) = isa(object, Metaobject)
+
+function compareMetaobjects(metaobject1, metaobject2, compared=Set{Tuple{Metaobject,Metaobject}})
     if metaobject1 === metaobject2 return true end
-    if length(metaobject1) != length(metaobject2) return false end
-
     if (metaobject1, metaobject2) in compared return true end
+
     push!(compared, (metaobject1, metaobject2))
+
     for i in 1:length(metaobject1)
         if isMetaobject(metaobject1[i]) && isMetaobject(metaobject2[i])
             if !compareMetaobjects(metaobject1[i], metaobject2[i], compared)
@@ -31,15 +62,12 @@ function compareMetaobjects(metaobject1::Vector{Any}, metaobject2::Vector{Any}, 
     return true
 end
 
-function ==(metaobject1::Vector, metaobject2::Vector)
+function ==(metaobject1::Metaobject, metaobject2::Metaobject)
     # In order to avoid infinite recursion
-    if isMetaobject(metaobject1) && isMetaobject(metaobject2)
-        return compareMetaobjects(metaobject1, metaobject2)
-    end
-    return isequal(metaobject1,metaobject2)
+    compareMetaobjects(metaobject1, metaobject2)
 end
 
-Class = [
+Class = Metaobject([
     nothing, # class_of
     :Class, # name
     [], # direct_superclasses
@@ -48,28 +76,59 @@ Class = [
     [:name, :direct_superclasses, :direct_slots, :cpl, :slots, :direct_subclasses, :direct_methods], # slots
     [], # direct_subclasses
     [] # direct_methods
-]
+])
 
 Class[1] = Class
 Class[5] = [Class]
 
-Top = [Class, :Top, [], [], [Class], [], [], []]
-Object = [Class, :Object, [Top], [], [Class], [], [], []]
+Top = Metaobject([
+    Class,
+    :Top, 
+    [], 
+    [], 
+    [Class], 
+    [], 
+    [], 
+    []
+])
+
+Object = Metaobject([
+    Class, 
+    :Object, 
+    [Top], 
+    [], 
+    [Class], 
+    [], 
+    [], 
+    []
+])
+
 Class[3] = [Object]
 
+function print_class(class, io)
+    print(io, "<$(class_name(class_of(class))) $(class_name(class))>")
+end
+
+function print_object(obj, io)
+    print(io, "<$(class_name(class_of(obj))) $(string(objectid(obj), base=62))>")
+end
+
+function Base.show(io::IO, ::MIME"text/plain", metaobject::Metaobject)
+    print_class(metaobject, io)
+end
 
 macro defclass(name, direct_superclasses, direct_slots)
     return quote
-        global $(esc(name)) = [
+        global $(esc(name)) = $(esc(Metaobject))([
             Class, # class_of
             $(QuoteNode(name)), # name
-            vcat($(esc(direct_superclasses)), [Object]), # direct_superclasses
+            vcat($(esc(direct_superclasses)), [$(esc(Object))]), # direct_superclasses
             $(esc(direct_slots)), # direct_slots
-            [], # cpl
+            [$(esc(Class))], # cpl
             $(esc(direct_slots)), # slots
             [], # direct_subclasses
             [], # direct_methods
-        ]
+        ])
     end
 end
 
@@ -81,12 +140,12 @@ macro defgeneric(expr)
     name = expr.args[1]
     lambdaList = expr.args[2:end]
     return quote
-        global $(esc(name)) = [
+        global $(esc(name)) = $(esc(Metaobject))([
             GenericFunction, # class_of
             $(QuoteNode(name)), # name
             $(esc(lambdaList)), # lambda-list
             [], # methods
-        ]
+        ])
     end
 end
 
@@ -126,47 +185,3 @@ function compute_cpl(instance)
     end
     return cpl
 end
-
-function Base.getproperty(instance::Vector, property::Symbol)
-    # instance.property resolves to:
-    # -> instance asking its class for the index of the property in the slots
-    # -> using this index to access its slots (+1 because of the first slots representing the class_of)
-    if length(instance) > 0 && isMetaobject(instance)
-        instance[1+findall(slot -> slot == property, class_of(instance)[6])[1]]
-    end
-end
-
-function Base.setproperty!(instance::Vector, property::Symbol, value)
-    # instance.property = value resolves to:
-    # -> instance asking its class for the index of the property in the slots
-    # -> using this index to access its slots (+1 because of the first slots representing the class_of)
-    # -> pushing a value
-    if length(instance) > 0 && isMetaobject(instance)
-        instance[1+findall(slot -> slot == property, class_of(instance)[6])[1]] = value
-    end
-end
-
-# Should specialize for Objects
-function print_object(obj, io)
-    print(io, "<$(class_name(class_of(obj))) $(string(objectid(obj), base=62))>")
-end
-
-function Base.show(io::IO, ::MIME"text/plain", object::Vector)
-    if (isMetaobject(object))
-        print_object(object, io)
-    else 
-        show(io, object)
-    end
-end
-
-# Introspection
-class_of(instance) = instance[1]
-class_name(instance) = instance.name
-class_direct_slots(instance) = instance.direct_slots
-class_slots(instance) = instance.slots
-class_direct_superclasses(instance) = instance.direct_superclasses
-class_cpl(instance) = instance.cpl
-class_direct_methods(instance) = instance.direct_methods
-
-generic_methods(generic_function) = generic_function.methods
-method_specializers(method) = method.specializers
